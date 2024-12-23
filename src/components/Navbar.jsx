a8c63c0ab5cc6e1afb67d6eb6394b8c509eefa33
MdShijanAli <freelancershijan@gmail.com>
2024-12-24 00:45:24 +0600

    Update: Dark Mode Issue Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,46 +51,57 @@ export default function Navbar() {
             }
           </li>
         </ul>
-        {/* Mobile Menu Button */}
-        <button
-          className="block md:hidden text-gray-700 hover:text-green-600 focus:outline-none"
-          onClick={toggleMenu}
-        >
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
+        <div className="flex items-center gap-4">
+          {/* Mobile Menu Button */}
+          <div onClick={handleSwitchTheme} className="cursor-pointer block md:hidden">
+            {
+              theme === 'dark' ? (
+                <LightMoodIcon className="dark:text-white" />
+              ) : (
+                <DarkMoodIcon className="dark:text-white" />
+              )
+            }
+          </div>
+          <button
+            className="block md:hidden text-gray-700 hover:text-green-600 focus:outline-none"
+            onClick={toggleMenu}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 6h16M4 12h16m-7 6h7"
-            />
-          </svg>
-        </button>
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
       {/* Mobile Menu */}
       {isMenuOpen && (
         <ul
-          className="absolute top-full left-0 w-full bg-white shadow-md z-10 md:hidden"
+          className="absolute top-full left-0 w-full bg-white dark:bg-[#0B1120] dark:border-b dark:border-gray-800 shadow-md z-10 md:hidden"
         >
           <li>
-            <a href="#" className="block text-gray-700 hover:text-green-600 p-4">
+            <a href="#" className="block text-gray-700 dark:text-white hover:text-green-600 p-4">
               Dashboard
             </a>
           </li>
           <li>
-            <a href="#" className="block text-gray-700 hover:text-green-600 p-4">
+            <a href="#" className="block text-gray-700 dark:text-white hover:text-green-600 p-4">
               Properties
             </a>
           </li>
           <li>
             <button
               onClick={toggleModal}
-              className="block text-gray-700 hover:text-green-600 p-4">
+              className="block text-gray-700 dark:text-white hover:text-green-600 p-4">
               Add Property
             </button>
           </li>
