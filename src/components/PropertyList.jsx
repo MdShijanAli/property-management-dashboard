b273b1cec6d0cd176d20e05d4bba162acc670ffb
MdShijanAli <freelancershijan@gmail.com>
2024-12-23 23:19:38 +0600

    Add: No Found Property Text Added

--- src/components/PropertyList.jsx
+++ src/components/PropertyList.jsx
@@ -19,27 +19,35 @@ export default function PropertyList() {
   });
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {filteredProperties.map((property) => (
-        <div key={property.id} className="bg-white p-10 rounded-xl shadow-md">
-          <p className="mb-3">
-            Date: <span className="font-semibold">{property.date || "N/A"}</span>
-          </p>
-          <h3 className="text-xl font-bold">{property.name}</h3>
-          <p className="leading-loose">
-            Type: <span className="font-semibold">{property.type || "N/A"}</span>
-          </p>
-          <p>
-            Status:{" "}
-            <span
-              className={`font-semibold ${ property.status === "Available" ? "text-green-600" : "text-red-600"
-                }`}
-            >
-              {property.status || "N/A"}
-            </span>
-          </p>
-        </div>
-      ))}
+    <div>
+      {
+        filteredProperties.length > 0 ?
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {filteredProperties.map((property) => (
+              <div key={property.id} className="bg-white p-10 rounded-xl shadow-md">
+                <p className="mb-3">
+                  Date: <span className="font-semibold">{property.date || "N/A"}</span>
+                </p>
+                <h3 className="text-xl font-bold">{property.name}</h3>
+                <p className="leading-loose">
+                  Type: <span className="font-semibold">{property.type || "N/A"}</span>
+                </p>
+                <p>
+                  Status:{" "}
+                  <span
+                    className={`font-semibold ${ property.status === "Available" ? "text-green-600" : "text-red-600"
+                      }`}
+                  >
+                    {property.status || "N/A"}
+                  </span>
+                </p>
+              </div>
+            ))}
+          </div> :
+          <div className="flex justify-center items-center h-96">
+            <p className="bg-gray-300 p-5 text-lg rounded-lg font-semibold">No Property Found!!! Please Create a Property From Navbar</p>
+          </div>
+      }
     </div>
   );
 }
