0d83fbe76b917c8241fffd9e737488804bafb01b
MdShijanAli <freelancershijan@gmail.com>
2024-12-24 00:39:50 +0600

    Update: Footer Border Color Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer(){
   return (
-    <footer className="bg-gray-100 dark:bg-gray-900 border-t border-black text-center py-4">
+    <footer className="bg-gray-100 dark:bg-gray-900 border-t dark:border-black text-center py-4">
       <p className="text-gray-600 dark:text-white">&copy; 2024 Property Management Dashboard</p>
     </footer>
   );
